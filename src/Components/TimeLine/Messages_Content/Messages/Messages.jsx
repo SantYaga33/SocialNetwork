a61c56b602231429bd02a5cda5fd45943ff5b63b
SantYaga33 <45719691+SantYaga33@users.noreply.github.com>
2020-02-21 17:33:21 +0400

    messages-chatarea

--- src/Components/TimeLine/Messages_Content/Messages/Messages.jsx
+++ src/Components/TimeLine/Messages_Content/Messages/Messages.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import styles from  './Messages.module.css';
+import img from './Friends_list/Friends_Img/3.png'
+import img1 from './Friends_list/Friends_Img/2.png'
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import Friends_list from "./Friends_list/Friends_list";
 
 
 
@@ -8,15 +12,89 @@ const Messages = () => {
 	return (
 		<div className={styles.messages_wrap}>
 			<div className={styles.messages	}>
-				<div className={styles.messages_title}></div>
+				<div className={styles.messages_titleWrap}>
+					<h5 className={styles.messages_title}>
+						<FontAwesomeIcon icon='bell'className={styles.messages_titleIcon} />All Messages
+					</h5>
+					<FontAwesomeIcon icon='ellipsis-h'className={styles.messages_titleIcon} />
+				</div>
 				<div className={styles.messages_infoWrap}>
-					<div className={styles.messages_friendsList}></div>
+					<div className={styles.messages_friendsList}>
+						<Friends_list/>
+					</div>
 					<div className={styles.messages_chat}>
+
 						<div className={styles.messages_chatHead}>
-							<div className={styles.chatHead_ava}></div>
-							<span></span>
+							<div  className={styles.chatHead_ava}>
+								<img src={img} alt=""/>
+							</div>
+							<div className={styles.chatHead_title}>
+								<h4> john snow </h4>
+								<span className={styles.chatHead_status}>online </span>
+							</div>
 						</div>
-						<div className={styles.messages_chatArea}></div>
+
+						<div className={styles.messages_chatArea}>
+							<ul>
+								<li className={styles.you}>
+									<div className={styles.chatArea_ava}>
+										<img src={img} alt=""/>
+									</div>
+									<p className={styles.chatArea_text}>text  text  text text</p>
+								</li>
+
+								<li className={styles.me}>
+									<div className={styles.chatArea_ava}>
+										<img src={img1} alt=""/>
+									</div>
+									<p className={styles.chatArea_text}> пыуаыа цыуцукцк цукцук</p>
+								</li>
+								<li className={styles.you}>
+									<div className={styles.chatArea_ava}>
+										<img src={img} alt=""/>
+									</div>
+									<p className={styles.chatArea_text}>text  text  text text</p>
+								</li>
+
+								<li className={styles.me}>
+									<div className={styles.chatArea_ava}>
+										<img src={img1} alt=""/>
+									</div>
+									<p className={styles.chatArea_text}> пыуаыа цыуцукцк цукцук</p>
+								</li>
+								<li className={styles.you}>
+									<div className={styles.chatArea_ava}>
+										<img src={img} alt=""/>
+									</div>
+									<p className={styles.chatArea_text}>text  text  text text</p>
+								</li>
+
+								<li className={styles.me}>
+									<div className={styles.chatArea_ava}>
+										<img src={img1} alt=""/>
+									</div>
+									<p className={styles.chatArea_text}> пыуаыа цыуцукцк цукцук</p>
+								</li>
+								<li className={styles.you}>
+									<div className={styles.chatArea_ava}>
+										<img src={img} alt=""/>
+									</div>
+									<p className={styles.chatArea_text}>text  text  text text</p>
+								</li>
+
+								<li className={styles.me}>
+									<div className={styles.chatArea_ava}>
+										<img src={img1} alt=""/>
+									</div>
+									<p className={styles.chatArea_text}> пыуаыа цыуцукцк цукцук</p>
+								</li>
+							</ul>
+						</div>
+
+
+
+
+
 						<div className={styles.messages_chatText}></div>
 					</div>
 				</div>
