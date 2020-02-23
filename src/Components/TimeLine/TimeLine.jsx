19612062d775136e2264662f3c1cd0cf0518b0f2
SantYaga33 <45719691+SantYaga33@users.noreply.github.com>
2020-02-23 22:24:37 +0400

    ??

--- src/Components/TimeLine/TimeLine.jsx
+++ src/Components/TimeLine/TimeLine.jsx
@@ -3,8 +3,9 @@ import styles from  './TimeLine.module.css';
 import Header from "./Header/Header";
 import TimeLine_Content from "./TimeLine_Content/TimeLine_Content";
 import Footer from "./Footer/Footer";
-import {Route} from "react-router-dom";
+
 import Messages_Content from "./Messages_Content/Messages_Content";
+import {Route} from "react-router-dom";
 
 
 
@@ -12,13 +13,13 @@ const TimeLine = (props) => {
 	return (
 		<div className={styles.timeLine}>
 			<Header headerNavLinks={props.headerNavLinks}/>
-			{/*<Route path='/' exact  render={ () => <TimeLine_Content /> }/>*/}
-			{/*<Route path='/Timeline'  render={ () => <TimeLine_Content /> }/>*/}
-			{/*<Route path='/Messages'  render={ () => <Messages_Content /> }/>*/}
+			<Route path='/' exact    render={ () => <TimeLine_Content /> }/>
+			<Route path='/Timeline'  render={ () => <TimeLine_Content /> }/>
+			<Route path='/Messages'  render={ () => <Messages_Content /> }/>
 			<Footer/>
 		</div>
 
 	);
-}
+};
 
 export default TimeLine;
